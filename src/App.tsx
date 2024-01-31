f548351db30a49f5acc2a6b8022946f54ecc2c9d
mnava1418 <martin@mnavapena.com>
2024-01-31 17:53:21 -0600

    UPDATE: add bootstrap and signUp page

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import PublicRoute from './components/auth/PublicRoute'
 import Login from './components/Login';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
+import SignUp from './components/pages/SignUp';
 
 import { PATHS } from './config/constants';
 
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const App = () => {
   return (
@@ -16,6 +18,7 @@ const App = () => {
       <Router>
         <Routes>
             <Route path={PATHS.login} element={<PublicRoute><Login /></PublicRoute>} />
+            <Route path={PATHS.signup} element={<PublicRoute><SignUp /></PublicRoute>} />
             <Route path={PATHS.home} element={<PrivateRoute><Home /></PrivateRoute>} />
             <Route path='*' element={<NotFound />} />
         </Routes>
