9fcd6b0391f22101acdd1d4f61156fdc8cbb64ce
mnava1418 <martin@mnavapena.com>
2024-02-01 18:11:06 -0600

    ADD: add signUp service method

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,3 +1,8 @@
 export const isAuthenticated = (): boolean => {
     return false
 }
+
+export const signUp = (email: string, password: string): void => {
+    console.log('email', email)
+    console.log('password', password)
+}
